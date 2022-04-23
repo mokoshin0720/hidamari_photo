0e395781f164125d4d0996b73807359fde565b22
Shinya Kinjo <65533712+mokoshin0720@users.noreply.github.com>
2022-04-23 14:08:17 +0900

    [add]import header image

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,12 @@
 import { Fragment, FC } from 'react';
 import Navigation from 'src/components/navigation'
+import TitleImage from 'src/assets/images/title.png'
 
 const Header: FC = () => {
     return (
         <Fragment>
-            <header>
-                <p>header</p>
+            <header className='bg-gray-200'>
+                <img src={TitleImage} alt="hidamari" className="w-1/4" />
                 <Navigation />
             </header>
         </Fragment>

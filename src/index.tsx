a9106113113ab8996463c4fec7bd8f437a7cbf82
Shinya Kinjo <65533712+mokoshin0720@users.noreply.github.com>
2022-05-02 15:52:02 +0900

    [add]normalize css

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "src/reportWebVitals";
 import Routing from "src/config/routing";
-import "src/index.css";
+import 'normalize.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement

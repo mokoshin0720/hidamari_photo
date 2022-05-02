43442992e641bfb0e0c0d73fff342e3bd2e50586
Shinya Kinjo <65533712+mokoshin0720@users.noreply.github.com>
2022-05-02 16:39:43 +0900

    [add]import background img

--- src/pages/photographer.tsx
+++ src/pages/photographer.tsx
@@ -1,6 +1,7 @@
 import { FC, Fragment } from "react";
 import Layout from "src/components/layout";
 import photographerImg from "src/assets/images/photographer/person.png";
+import style from "src/pages/photographer.module.css";
 
 const Photographer: FC = () => {
   const communication = `お子様にもご家族の皆様にも安心して楽しんでいただけるよう\nたくさんコミュニケーションをとりながら撮影致します。`
@@ -8,14 +9,16 @@ const Photographer: FC = () => {
   return (
     <Fragment>
       <Layout>
-        <h1>フォトグラファー</h1>
-        <img src={photographerImg} alt="" />
+        <div className={style.background}>
+          <h1>フォトグラファー</h1>
+          <img src={photographerImg} alt="" />
 
-        <div>
-          <h2>かなた あかり</h2>
-          <p>子ども写真館での店長経験あり。</p>
-          <p>お子様一人一人と向き合い、自然な表情を引き出します♪</p>
-          <p className="whitespace-pre-wrap">{communication}</p>
+          <div>
+            <h2>かなた あかり</h2>
+            <p>子ども写真館での店長経験あり。</p>
+            <p>お子様一人一人と向き合い、自然な表情を引き出します♪</p>
+            <p className="whitespace-pre-wrap">{communication}</p>
+          </div>
         </div>
       </Layout>
     </Fragment>

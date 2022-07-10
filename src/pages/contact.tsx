702bae493cc7b49578bb2ff1ed86b2cdb4286731
Shinya Kinjo <65533712+mokoshin0720@users.noreply.github.com>
2022-07-10 10:25:17 +0900

    emailjs skelton

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,8 +1,47 @@
-import { FC, Fragment } from "react";
+import { FC, Fragment, useState } from "react";
 import Layout from "src/components/layout";
 import style from "src/pages/contact.module.css";
+import { init, send } from 'emailjs-com';
 
 const Contact: FC = () => {
+  const [name, setName] = useState('')
+  const [mail, setMail] = useState('')
+  const [title, setTitle] = useState('')
+  const [message, setMessage] = useState('')
+
+  const sendMail = (e:any) => {
+    console.log("here")
+
+    e.preventDefault()
+    const userID = process.env.REACT_APP_USER_ID;
+    const serviceID = process.env.REACT_APP_SERVICE_ID;
+    const templateID = process.env.REACT_APP_TEMPLATE_ID;
+
+    if (
+      userID !== undefined &&
+      serviceID !== undefined &&
+      templateID !== undefined
+    ) {
+      init(userID)
+
+      const template_param = {
+        to_name: name,
+        from_email: mail,
+        title: title,
+        message: message,
+      }
+
+      send(serviceID, templateID, template_param).then(() => {
+        window.alert("お問い合わせを送信しました。")
+
+        setName('')
+        setMail('')
+        setMessage('')
+        setTitle('')
+      })
+    }
+  }
+
   return (
     <Fragment>
       <Layout>
@@ -16,13 +55,13 @@ const Contact: FC = () => {
           <div>
             <label htmlFor="">お名前（必須）</label>
             <br />
-            <input type="text" className={style.inputText} />
+            <input type="text" className={style.inputText} value={name} onChange={(e) => setName(e.target.value)} />
           </div>
 
           <div>
             <label htmlFor="">メールアドレス（必須）</label>
             <br />
-            <input type="text" className={style.inputText} />
+            <input type="text" className={style.inputText} value={mail} onChange={(e) => setMail(e.target.value)}  />
           </div>
 
           <div>
@@ -34,17 +73,17 @@ const Contact: FC = () => {
           <div>
             <label htmlFor="">撮影される方のお名前・年齢</label>
             <br />
-            <input type="text" className={style.inputText} />
+            <input type="text" className={style.inputText} value={title} onChange={(e) => setTitle(e.target.value)} />
           </div>
 
           <div>
             <label htmlFor="">ご質問・ご希望内容</label>
             <br />
-            <textarea placeholder="日時、シチュエーション、ご準備いただくものなどぜひご相談ください♪" />
+            <textarea placeholder="日時、シチュエーション、ご準備いただくものなどぜひご相談ください♪" value={message} onChange={(e) => setMessage(e.target.value)} />
           </div>
 
           <div className={style.submitContainer}>
-            <input type="submit" className={style.submit} />
+            <input type="submit" className={style.submit} onClick={sendMail} />
           </div>
         </form>
         </div>
